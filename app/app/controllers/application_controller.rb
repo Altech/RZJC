ed6eebe6ab4ce28d61c9d127302ebb18cdf21d77
Altech <takeno.sh@gmail.com>
2014-07-07 13:28:26 +0000

    リクエストのロギングコードを追加

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :logging_access
+
+  def logging_access
+    return if not Rails.env.production?
+    logger = Fluent::Logger::FluentLogger.new(nil, :host => 'vs-seed.net:8888')
+    logger.post('production.rzjc.access',
+                method: request.method,
+                path: request.fullpath,
+                referer: request.referer,
+                agent: request.user_agent,
+                ip: request.ip)
+  end
 end
